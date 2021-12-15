985020547efdd94cc89b6b8a54a53dfe4973e631
Debolina Chandra <debolinachandra18@gmail.com>
2021-12-15 16:02:19 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from 'react'
 import Todo from './components/Todo'
 import EditTodoList from './components/EditTodoList'
-import {Routes,Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <>
-     <Todo/>
-      <Routes>     
-      <Route path="/edit/:id">
-        <EditTodoList />
-      </Route>
-      </Routes>
-    </>
+    <div className="main_div">
+      <div className="child_div">
+        <figure>
+          <figcaption>Add your list here</figcaption>
+        </figure>
+        <div className="addItems">
+        <input type="text" placeholder=" ✍️ Add Items"/>
+         <i className="fa fa-plus add-btn"></i> 
+          <Todo />
+          <Routes>
+            <Route path="/edit/:id">
+              <EditTodoList />
+            </Route>
+          </Routes>
+        </div>
+      </div>
+    </div>
   )
 }
 
